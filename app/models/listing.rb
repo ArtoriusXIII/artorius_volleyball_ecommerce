bb72e744bd94f1f012a71ef4adefeae1f1f3240a
ArtoriusXIII <arthur.janka@gmail.com>
2016-07-28 11:41:33 +0000

    Vallidation for Users & Listings

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,7 @@ class Listing < ActiveRecord::Base
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
     
     belongs_to :category
+    
+    validates :name, :description, :price, presence: true
+    validates :price, numericality: {greater_than: 0}
 end
