919125e2913ff5c14bf98112084ff0344bdbd7dc
ArtoriusXIII <arthur.janka@gmail.com>
2016-08-01 14:54:43 +0000

    fixing deploying to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:account_update) << :username
-   
-  end
-  
   before_action :find_categories
+  
+  
+  
   
   def initialize_cart
     @cart = Cart.build_from_hash session
@@ -30,6 +23,13 @@ class ApplicationController < ActionController::Base
       redirect_to root_path, alert: "I'm sorry but action not allowed!."
     end
     
+  end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.for(:account_update) << :username
+   
   end
   
 end
