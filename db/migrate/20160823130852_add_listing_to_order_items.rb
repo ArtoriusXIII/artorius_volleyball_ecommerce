dd5a0860afe23e4cf2ac027a3910b3d9770e823f
ArtoriusXIII <arthur.janka@gmail.com>
2016-08-23 14:20:25 +0000

    remove old migration

--- db/migrate/20160823130852_add_listing_to_order_items.rb
+++
@@ -1,5 +0,0 @@
-class AddListingToOrderItems < ActiveRecord::Migration
-  def change
-    add_reference :order_items, :listing, index: true, foreign_key: true
-  end
-end
--- db/migrate/20160823131623_remove_product_from_order_items.rb
+++
@@ -1,5 +0,0 @@
-class RemoveProductFromOrderItems < ActiveRecord::Migration
-  def change
-    remove_reference :order_items, :product, index: true, foreign_key: true
-  end
-end
